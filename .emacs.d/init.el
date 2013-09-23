e7d6c7ea9b595dc35227035b6f1d5cbb22926cad
Earnestly <zibeon@gmail.com>
2013-09-23 23:12:19 +0100

    Add haskell-mode for .hs and .lhs files.

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -2,7 +2,14 @@
 (add-to-list 'package-archives 
              '("melpa" . "http://melpa.milkbox.net/packages/") t)
 
-(load-theme 'zenburn t)
 (menu-bar-mode -1)
+(tool-bar-mode -1)
+(scroll-bar-mode -1)
+
+; haskell-mode for .hs and .lhs (literate)
+(add-to-list 'auto-mode-alist '("^\\.?hs" . haskell-mode))
+(add-to-list 'auto-mode-alist '("^\\.?lhs" . haskell-mode))
+
+(load-theme 'zenburn t)
 
 (setq vc-follow-symlinks t)
