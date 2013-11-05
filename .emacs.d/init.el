e012f5cca7ae7094ed52c3f83aea25d8081f3d47
Earnestly <zibeon@gmail.com>
2013-11-05 11:26:07 +0000

    Use wombat instead.

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -6,12 +6,13 @@
 (tool-bar-mode -1)
 (scroll-bar-mode -1)
 
-; haskell-mode for .hs and .lhs (literate)
+;; haskell-mode for .hs and .lhs (literate)
 (add-to-list 'auto-mode-alist '("^\\.?hs" . haskell-mode))
 (add-to-list 'auto-mode-alist '("^\\.?lhs" . haskell-mode))
 
-(load-theme 'zenburn t)
+(load-theme 'wombat t)
 
 (setq vc-follow-symlinks t
       backup-inhibited t
       auto-save-default nil)
+(put 'scroll-left 'disabled nil)
