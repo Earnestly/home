a2ed99d2334dc7f38010e2f982f3068997b07da1
Earnestly <zibeon@gmail.com>
2013-11-13 21:49:36 +0000

    Written mostly by @holomorph. This tool allows me to remove a lot of duplicate code and keymappings with internal logic used based on the url itself.

---
+++ .config/pentadactyl/plugins/launchv.js
@@ -0,0 +1,54 @@
+/* Quick function to help reduce duplication in my config, thanks mostly to
+   holomorph who did pretty much all of this ❤ 
+
+   Adds two new commands and two new hint modes:
+   * :launchv  - Opens the current buffer url prefering yt-dl
+   * :launchvq - Same as above but prefers quvi
+   
+   Hints: 
+   * ;u - launches the current hint prefering yt-dl
+   * ;q - Same but prefers quvi
+*/
+ 
+function launchv(target, quvi=false) {
+
+    /* Escape anything which could be used to inject shell commands before
+     * passing it to the commands */
+    var uri = target.replace(/([$`"\\])/g, "\\$1");
+    var cmd;
+
+    function exec(launcher, uri) {
+        return commands.execute(launcher + ' "' + uri + '" &');
+    }
+
+    /* filter certain urls to more appropriate programs before passing to
+     * quvi */
+    if(uri.match(/twitch\.tv/))
+        exec("!lstream", uri);
+    else if(uri.match(/twitch\.tv\/.*\/c\/[0-9]+/))
+        exec("!yt-dl", uri);
+
+    /* Open youtube playlists of any kind directly with mpv */
+    else if(uri.match(/youtube.*[?&]list=PL/))
+        exec("!mpv", uri);
+
+    /* For everything else */
+    else if(quvi)
+        exec("!quvi dump -b mute", uri);
+    else
+        exec("!yt-dl", uri);
+}
+
+hints.addMode("q", "Launch video from hint (quvi)",
+    function (elem, loc) launchv(loc));
+
+hints.addMode("u", "Launch video from hint (yt-dl)",
+    function (elem, loc) launchv(loc, quvi=false));
+
+group.commands.add(["launchv", "lv"], "Launches current buffer video (yt-dl).",
+    function(args) { var uri = buffer.URL.replace(/([$`"\\])/g, "\\$1");
+        launchv(uri, quvi=false); });
+
+group.commands.add(["launchvq", "lvq"], "Launches current buffer video (quvi).",
+    function(args) { var uri = buffer.URL.replace(/([$`"\\])/g, "\\$1");
+        launchv(uri); });
