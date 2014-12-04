651b561e28c54516be34a660a40d0b14fdc7a75e
Earnestly <zibeon@gmail.com>
2014-12-04 18:51:03 +0000

    pentadactyl: Make launchv work with vimperator as well if needed.

--- .config/pentadactyl/plugins/launchv.js
+++ .config/pentadactyl/plugins/launchv.js
@@ -3,28 +3,26 @@
     Quick function to help reduce duplication in my config, thanks mostly to
     holomorph who started pretty much all of this.
 
-    Requires: livestreamer, mpv, youtube-dl (yt-dl).
+    Requires: livestreamer, mpv, youtube-dl.
 
-    Command:
-    * :launchv  Opens the current buffer url prefering yt-dl.
+    mpvand:
+    * :launchv  Attempts to start the current buffer URL as a video.
 
     Hint:
-    * ;q        Launches the current hint with yt-dl.
-
-    Note: If the url contains a youtube playlist it will return the playlist's
-          url rather than the direct youtube video.
+    * ;q        Likewise but for the selected hint.
 */
 
 function launchv(target){
-    /* Escape anything which could be used to inject shell commands before
-     * passing it to the commands. */
+    /* Escape anything which could be used to inject shell mpvands before
+     * passing it to the mpvands. */
     var uri = target.replace(/([$`"\\])/g, "\\$1");
+    var mpv = "mpv --loop-file --cache-file=TMP";
 
     function exec(launcher, uri){
 
-        /* If we're using dactyl then echo the action as io.system won't. */
+        /* If we're using pentadactyl then echo the action as io.system won't. */
         if(typeof dactyl !== "undefined")
-            dactyl.echomsg("Launching: " + launcher + " " + uri);
+            dactyl.echomsg("Executing: " + launcher + " \"" + uri + "\"");
 
         return io.system(launcher + ' "' + uri + '" &');
     }
@@ -41,30 +39,18 @@ function launchv(target){
     /* Currently youtube-dl's support for hitbox VOD is buggy but adding for
      * future. */
     else if(uri.match(/hitbox\.tv\/video\/[0-9]+/))
-        exec("mpv --loop-file --cache-file=TMP", uri);
+        exec(mpv, uri);
     else if(uri.match(/twitch\.tv/) || uri.match(/hitbox\.tv/))
         exec("livestreamer", uri);
-    else if(uri.match(/youtube.*[?&]list=(?:RD|UU)/))
-            exec("mpv --loop-file --cache-file=TMP", uri);
 
-    /* Open youtube playlists of any kind directly with mpv. */
-    else if(uri.match(/youtube.*[?&]list=PL/)){
-
-        /* Check if the url is part of a playlist but a direct video (watch?v=)
-         * url is provided and return the real playlist url. */
-        if(uri.match(/watch\?v=/)){
-            var uri = uri.replace(/watch\?v.+?\&/, "playlist\?")
-            exec("mpv", uri);
-        }
-
-    /* For everything else */
-    }else
-        exec("mpv --loop-file --cache-file=TMP", uri);
+    /* For everything else. */
+    else
+        exec(mpv, uri);
 }
 
 hints.addMode("q", "Launch video from hint", function (elem, loc) launchv(loc));
 
-group.commands.add(["launchv", "lv"], "Launches current buffer video",
+commands.add(["launchv", "lv"], "Launches current buffer video",
     function(args){ 
         var uri = buffer.URL;
         launchv(uri);
