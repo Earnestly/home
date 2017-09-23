b0f780cb162f8580d731de5204b804efd6040cba
Earnestly <zibeon@googlemail.com>
2017-09-23 11:55:10 +0100

    qutebrowser: initial configuration

---
+++ local/cfg/qutebrowser/config.py
@@ -0,0 +1,69 @@
+# LOCALDIR/cfg/qutebrowser/config.py
+
+config.load_autoconfig = False
+
+# General
+config.set('backend', 'webengine')
+
+config.set('editor.command', ['visual', '{}'])
+
+config.set('downloads.position', 'bottom')
+
+config.set('url.default_page', 'about:blank')
+config.set('url.start_pages', ['about:blank'])
+config.set('url.searchengines', {'DEFAULT': 'https://duckduckgo.com/html/?q=!{}'})
+
+config.set('content.dns_prefetch', False)
+config.set('content.javascript.enabled', False)
+
+config.set('tabs.background', True)
+config.set('tabs.last_close', 'close')
+config.set('tabs.show', 'multiple')
+
+config.set('hints.chars', 'fdsartgbvecwxqyiopmnhzuljk')
+config.set('hints.scatter', False)
+config.set('hints.uppercase', True)
+
+# Colours
+color_background = '#343d46'
+color_foreground = '#65737e'
+color_text = '#c0c5ce'
+color_red = '#cc6666'
+
+config.set('colors.tabs.indicator.system', 'none')
+config.set('colors.tabs.bar.bg', color_background)
+config.set('colors.tabs.odd.bg', color_background)
+config.set('colors.tabs.odd.fg', color_foreground)
+config.set('colors.tabs.even.bg', color_background)
+config.set('colors.tabs.even.fg', color_foreground)
+config.set('colors.tabs.selected.odd.fg', color_text)
+config.set('colors.tabs.selected.odd.bg', color_background)
+config.set('colors.tabs.selected.even.fg', color_text)
+config.set('colors.tabs.selected.even.bg', color_background)
+config.set('colors.tabs.indicator.start', color_text)
+config.set('colors.tabs.indicator.stop', color_background)
+config.set('colors.tabs.indicator.error', color_red)
+
+# Fonts
+font_ui = '10pt Interface'
+font_mono = '11.5pt Inconsolatazi4'
+
+config.set('fonts.tabs', font_ui)
+config.set('fonts.statusbar', font_mono)
+config.set('fonts.completion.entry', font_mono)
+config.set('fonts.completion.category', font_ui)
+
+# Key Bindings
+config.unbind('O')
+
+config.bind('<Alt-h>', 'tab-prev')
+config.bind('<Alt-l>', 'tab-next')
+
+config.bind('U', 'spawn -vd mpv-url {url}')
+config.bind(';q', 'hint links spawn -vd mpv-url {hint-url}')
+
+config.bind('t', 'set-cmd-text -s :open -t')
+config.bind('O', 'set-cmd-text :open {url}')
+
+config.bind('<Alt-x>', 'set -p content.javascript.enabled!')
+config.bind('<Ctrl-Shift-p>', 'set -p content.private_browsing!')
