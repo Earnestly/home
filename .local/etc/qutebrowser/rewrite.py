74279d704d372ce7d8e0dd3bc0a896d81920da2c
Earnestly <zibeon@googlemail.com>
2021-03-13 12:07:18 +0000

    qutebrowser: rewrite: use teddit.com for reddit urls

--- .local/etc/qutebrowser/rewrite.py
+++ .local/etc/qutebrowser/rewrite.py
@@ -4,7 +4,7 @@ def rewrite(request: qutebrowser.api.interceptor.Request):
     url = request.request_url.host()
 
     if url == 'reddit.com' or url == 'www.reddit.com':
-        request.request_url.setHost('old.reddit.com')
+        request.request_url.setHost('teddit.net')
         request.redirect(request.request_url)
 
     if request.request_url.host().startswith('www.amazon.') and '/dp/' in request.request_url.path():
