22715c317c8b4c93ad3c5ba4a965cd7762919e81
Earnestly <Earnestly@github.com>
2024-12-23 11:46:12 +0000

    mpv: replace keybind with quirks script

--- .local/etc/mpv/scripts/quirks.lua
+++ .local/etc/mpv/scripts/quirks.lua
@@ -9,3 +9,11 @@ mp.observe_property("metadata", "native", function(_, metadata)
         end
     end
 end)
+
+-- Used for my x220 thinkpad (HD2000). Both GPU usage (hwdec=vaapi) and
+-- temperatures rapidly increase when using bt.601-525 RGB primaries.
+mp.observe_property("video-params/primaries", "string", function(_, primaries)
+    if primaries and primaries == "bt.601-525" then
+        mp.commandv("vf", "append", "format=primaries=bt.709")
+    end
+end)
