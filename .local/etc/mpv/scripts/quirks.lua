dcdf9bc6178850c61fb0dce14e28ab9c01d87550
Earnestly <Earnestly@github.com>
2025-12-15 17:05:38 +0000

    mpv: twitch ad detection improvements

--- .local/etc/mpv/scripts/quirks.lua
+++ .local/etc/mpv/scripts/quirks.lua
@@ -1,5 +1,6 @@
 -- HOME/.local/etc/mpv/scripts/quirks.lua
 
+-- Hacks for specific path specific hacks.
 mp.observe_property("path", "string", function(_, path)
     if path and path:find("https://www.twitch.tv/", 1, true) then
         -- XXX May not need this function.
@@ -30,39 +31,45 @@ mp.observe_property("path", "string", function(_, path)
             local module = message.prefix
             local content = string.gsub(message.text, "\n*$", "")
 
-            -- It appears to be possible to bypass ads on twitch.tv by
-            -- reloading the stream when an ad is intending to play.  Various
-            -- messages are used as sentinels to detect this condition; there
-            -- may be better ways.
-
-            -- XXX Preroll ads cause some difficulty.
+            -- It appears to be possible to bypass advertss on twitch.tv by
+            -- reloading the stream when an advert is intending to play.
+            -- Various messages are used as sentinels to detect this condition;
+            -- there may be better ways.
             if module == "ffmpeg/demuxer" and fgrep({"hls: Packet corrupt (stream = 1, dts ="}, content) then
                 mp.osd_message("[twitch]: Advert incoming", 4)
             end
 
-            if module == "ad" and fgrep({"Invalid audio PTS:"}, content) then
-                mp.osd_message("[twitch]: Reloading stream", 4)
-                mp.command("playlist-play-index current")
-            end
+            -- Using the audio timestamps warnings appear to be the best way to
+            -- detect an advert so far.
+            if module == "ad" then
+                local jump, _, from, to = content:find("Invalid audio PTS: (%d+)%.%d+ %-> (%d+)%.%d+")
 
-            -- These triggers make it impossible to jump past preroll ads.
-            -- local triggers = {
-            --     "h264: co located POCs unavailable",
-            --     "h264: mmco: unref short failure"
-            -- }
-            --
-            -- if module == "ffmpeg/video" and fgrep(triggers, content) then
-            --     mp.osd_message("[twitch]: Reloading stream", 2)
-            --     mp.command("playlist-play-index current")
-            -- end
+                from = tonumber(from)
+                to = tonumber(to)
+
+                -- NB. Preroll ads tend to always jump from 75.00xxx. Skipping
+                --     a reload on that specific timestamp prevents infinite
+                --     restarts.
+                if jump and from ~= 75 and from < to then
+                    mp.osd_message("[twitch]: Reloading stream", 4)
+                    mp.command("playlist-play-index current")
+                end
+            end
 
             -- Some twitch.tv streams do not provide formats that can be
             -- downloaded when using live-from-start so this attempts to reload
             -- the stream without it.
-            if module == "ytdl_hook" and fgrep({"use --no-live-from-start"}, content) then
-                mp.osd_message("[yt-dlp]: live-from-start not available", 4)
-                mp.command("no-osd change-list ytdl-raw-options remove live-from-start; \
-                            playlist-play-index current")
+            if module == "ytdl_hook" then
+                local triggers = {
+                    "use --no-live-from-start",
+                    "You must be logged into an account"
+                }
+
+                if fgrep(triggers, content) then
+                    mp.osd_message("[yt-dlp]: live-from-start not available", 4)
+                    mp.command("no-osd change-list ytdl-raw-options remove live-from-start; \
+                                playlist-play-index current")
+                end
             end
         end)
     end
