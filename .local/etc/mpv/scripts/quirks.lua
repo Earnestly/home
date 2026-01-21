75ec44018a8b4aba8d86fd6388bb97c6e6615741
Earnestly <Earnestly@github.com>
2026-01-21 21:09:59 +0000

    mpv: refine ad detection test condition

--- .local/etc/mpv/scripts/quirks.lua
+++ .local/etc/mpv/scripts/quirks.lua
@@ -17,7 +17,7 @@ mp.observe_property("path", "string", function(_, path)
         -- If ytdl_description is present for twitch.tv streams then use it as
         -- the title instead of the current date. Some streams already set the
         -- media-title to this value in which case ytdl_description is not
-        -- present.  In either case also prepend the streamer's username to the
+        -- present. In either case also prepend the streamer's username to the
         -- title.
         mp.observe_property("metadata", "native", function(_, metadata)
             if metadata then
@@ -31,15 +31,15 @@ mp.observe_property("path", "string", function(_, path)
             local module = message.prefix
             local content = string.gsub(message.text, "\n*$", "")
 
-            -- It appears to be possible to bypass advertss on twitch.tv by
-            -- reloading the stream when an advert is intending to play.
-            -- Various messages are used as sentinels to detect this condition;
-            -- there may be better ways.
+            -- It appears possible to bypass adverts on twitch.tv by reloading
+            -- the stream when an advert is intending to play. Various messages
+            -- are used as sentinels to detect this condition but there may be
+            -- better ways.
             if module == "ffmpeg/demuxer" and fgrep({"hls: Packet corrupt (stream = 1, dts ="}, content) then
                 mp.osd_message("[twitch]: Advert incoming", 4)
             end
 
-            -- Using the audio timestamps warnings appear to be the best way to
+            -- Using audio timestamp warnings appear to be the best way to
             -- detect an advert so far.
             if module == "ad" then
                 local jump, _, from, to = content:find("Invalid audio PTS: (%d+)%.%d+ %-> (%d+)%.%d+")
@@ -49,9 +49,9 @@ mp.observe_property("path", "string", function(_, path)
 
                 -- nb. Preroll ads tend to always jump from 75.00xxx. Skipping
                 --     a reload on that specific timestamp prevents infinite
-                --     restarts. However if the jump is from 75 (or any other
-                --     value) to 60 then a reload should trigger.
-                if jump and ((from == 75 and to == 60) or to == 60) or (from ~= 75 and from < to) then
+                --     restarts unless the jump is to 60.00xxx in which case
+                --     the reload should proceed.
+                if jump and (from ~= 75 or to == 60) then
                     mp.osd_message("[twitch]: Reloading stream", 4)
                     mp.command("playlist-play-index current")
                 end
@@ -60,6 +60,10 @@ mp.observe_property("path", "string", function(_, path)
             -- Some twitch.tv streams do not provide formats that can be
             -- downloaded when using live-from-start so this attempts to reload
             -- the stream without it.
+            --
+            -- nb. This is part of the advert skipping strategy; the stream
+            --     itself does not actually start from the beginning if the
+            --     formats are actually available.
             if module == "ytdl_hook" then
                 local triggers = {
                     "use --no-live-from-start",
