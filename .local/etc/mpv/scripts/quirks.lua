29403494cfe7e0a03f01da7759b8f32a2b16db95
Earnestly <Earnestly@github.com>
2026-01-23 11:17:58 +0000

    mpv: only apply ad skipping to live streams

--- .local/etc/mpv/scripts/quirks.lua
+++ .local/etc/mpv/scripts/quirks.lua
@@ -1,6 +1,15 @@
--- HOME/.local/etc/mpv/scripts/quirks.lua
+-- XDG_CONFIG_HOME/mpv/scripts/quirks.lua
 
--- Hacks for specific path specific hacks.
+-- For my x220 thinkpad (HD2000). Both GPU usage (hwdec=vaapi) and temperatures
+-- rise rapidly when the bt.601-525 RGB primaries are in effect. Switching to
+-- bt.709 calms it back down.
+mp.observe_property("video-params/primaries", "string", function(_, primaries)
+    if primaries and primaries == "bt.601-525" then
+        mp.command("no-osd vf append format=primaries=bt.709")
+    end
+end)
+
+-- Quirks for specific paths.
 mp.observe_property("path", "string", function(_, path)
     if path and path:find("https://www.twitch.tv/", 1, true) then
         -- XXX May not need this function.
@@ -26,65 +35,58 @@ mp.observe_property("path", "string", function(_, path)
             end
         end)
 
-        mp.enable_messages("warn")
-        mp.register_event("log-message", function(message)
-            local module = message.prefix
-            local content = string.gsub(message.text, "\n*$", "")
+        if not path:find("https://www.twitch.tv/videos/", 1, true) then
+            mp.enable_messages("warn")
+            mp.register_event("log-message", function(message)
+                local module = message.prefix
+                local content = string.gsub(message.text, "\n*$", "")
 
-            -- It appears possible to bypass adverts on twitch.tv by reloading
-            -- the stream when an advert is intending to play. Various messages
-            -- are used as sentinels to detect this condition but there may be
-            -- better ways.
-            if module == "ffmpeg/demuxer" and fgrep({"hls: Packet corrupt (stream = 1, dts ="}, content) then
-                mp.osd_message("[twitch]: Advert incoming", 4)
-            end
-
-            -- Using audio timestamp warnings appear to be the best way to
-            -- detect an advert so far.
-            if module == "ad" then
-                local jump, _, from, to = content:find("Invalid audio PTS: (%d+)%.%d+ %-> (%d+)%.%d+")
-
-                from = tonumber(from)
-                to = tonumber(to)
-
-                -- nb. Preroll ads tend to always jump from 75.00xxx. Skipping
-                --     a reload on that specific timestamp prevents infinite
-                --     restarts unless the jump is to 60.00xxx in which case
-                --     the reload should proceed.
-                if jump and (from ~= 75 or to == 60) then
-                    mp.osd_message("[twitch]: Reloading stream", 4)
-                    mp.command("playlist-play-index current")
+                -- It appears possible to bypass adverts on twitch.tv by reloading
+                -- the stream when an advert is intending to play. Various messages
+                -- are used as sentinels to detect this condition but there may be
+                -- better ways.
+                if module == "ffmpeg/demuxer" and fgrep({"hls: Packet corrupt (stream = 1, dts ="}, content) then
+                    mp.osd_message("[twitch]: Advert incoming", 4)
                 end
-            end
 
-            -- Some twitch.tv streams do not provide formats that can be
-            -- downloaded when using live-from-start so this attempts to reload
-            -- the stream without it.
-            --
-            -- nb. This is part of the advert skipping strategy; the stream
-            --     itself does not actually start from the beginning if the
-            --     formats are actually available.
-            if module == "ytdl_hook" then
-                local triggers = {
-                    "use --no-live-from-start",
-                    "You must be logged into an account"
-                }
+                -- Using audio timestamp warnings appear to be the best way to
+                -- detect an advert so far.
+                if module == "ad" then
+                    local jump, _, from, to = content:find("Invalid audio PTS: (%d+)%.%d+ %-> (%d+)%.%d+")
 
-                if fgrep(triggers, content) then
-                    mp.osd_message("[yt-dlp]: live-from-start not available", 4)
-                    mp.command("no-osd change-list ytdl-raw-options remove live-from-start; \
-                                playlist-play-index current")
+                    from = tonumber(from)
+                    to = tonumber(to)
+
+                    -- nb. Preroll ads tend to always jump from 75.00xxx. Skipping
+                    --     a reload on that specific timestamp prevents infinite
+                    --     restarts unless the jump is to 60.00xxx in which case
+                    --     the reload should proceed.
+                    if jump and (from ~= 75 or to == 60) then
+                        mp.osd_message("[twitch]: Reloading stream", 4)
+                        mp.command("playlist-play-index current")
+                    end
                 end
-            end
-        end)
-    end
-end)
 
--- For my x220 thinkpad (HD2000). Both GPU usage (hwdec=vaapi) and temperatures
--- rise rapidly when the bt.601-525 RGB primaries are in effect. Switching to
--- bt.709 calms it back down.
-mp.observe_property("video-params/primaries", "string", function(_, primaries)
-    if primaries and primaries == "bt.601-525" then
-        mp.command("no-osd vf append format=primaries=bt.709")
+                -- Some twitch.tv streams do not provide formats that can be
+                -- downloaded when using live-from-start so this attempts to reload
+                -- the stream without it.
+                --
+                -- nb. This is part of the advert skipping strategy; the stream
+                --     itself does not actually start from the beginning if the
+                --     formats are actually available.
+                if module == "ytdl_hook" then
+                    local triggers = {
+                        "use --no-live-from-start",
+                        "You must be logged into an account"
+                    }
+
+                    if fgrep(triggers, content) then
+                        mp.osd_message("[yt-dlp]: live-from-start not available", 4)
+                        mp.command("no-osd change-list ytdl-raw-options remove live-from-start; \
+                                    playlist-play-index current")
+                    end
+                end
+            end)
+        end
     end
 end)
